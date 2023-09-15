b86b8f284fbb56eea3e7993a6245a741b0973c83
hiep.nguyen <hiep.nguyen@icomm.vn>
2023-09-15 20:34:04 +0700

    up portfolio mobile

--- src/views/components/header/header.tsx
+++ src/views/components/header/header.tsx
@@ -8,7 +8,7 @@ function Header({ background, avatar, name, description }) {
         <div className="w-full bg-neutral-900 h-3/4">
           <img src={background} alt="Wallpaper" className="h-full mx-auto" />
         </div>
-        <div className="mt-[-30px] sm:mt-[-60px] ml-[5%] sm:ml-[10%] flex items-center ">
+        <div className="mt-[-30px] sm:mt-[-60px] mx-[5%] sm:mx-[10%] flex items-center ">
           <img src={avatar} alt="Avatar" className="rounded-full w-36 h-36 sm:w-40 sm:h-40 " />
           <div className="flex flex-col mt-16 ml-4 space-y-1">
             <span className="text-3xl username-desktop">{name}</span>
