f24b70fd032ce82bd94ee8b1384240fa208ab681
hiep.nguyen <hiep.nguyen@icomm.vn>
2023-09-24 02:46:57 +0700

    update favicon

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Helmet>
       <meta charSet="utf-8" />
       <title>On The Desk</title> 
-      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico?"></link>
+      <link rel="icon" href="/on-the-desk/favicon.ico?"/>
     </Helmet>
     <div className="flex flex-col h-[100vh]">
       <div className="flex-1 overflow-auto bg-[#18191A] ">
