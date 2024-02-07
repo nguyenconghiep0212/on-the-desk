0de212ee3bed7ced799f11a9739034c8db95a15b
hiep.nguyen <hiep.nguyen@icomm.com>
2024-02-07 17:59:01 +0700

    up fake data

---
+++ src/assests/data.ts
@@ -0,0 +1,313 @@
+const profile = {
+    "name": "Admin",
+    "email": null,
+    "description": "Ex-President of the United State of America",
+    "shortcut": "@admin",
+    "job": null,
+    "avatar": "https://cdn.onthedesk.vn/@admin/Avatar/223b562e4f1f448f9f36d128f2e2b547.png",
+    "contacts": [
+        {
+            "id": "654be881d74412edf1aef48a",
+            "typeContact": "phone",
+            "keyContact": "phone",
+            "nameContact": "Số điện thoại",
+            "infoDetail": "0949466789",
+            "linkIcon": "/images/contact/ic-phonecall.svg",
+            "backgoundColor": "#0D7B2C",
+            "backgroundColor": "#0D7B2C",
+            "templateId": "654369c94c2fd7e46ecdf278",
+            "status": 1
+        },
+        {
+            "id": "655af335100530f91533a6d8",
+            "typeContact": "social",
+            "keyContact": "zalo",
+            "nameContact": "Zalo",
+            "infoDetail": "0931141297",
+            "linkIcon": "/images/contact/ic-zalo.svg",
+            "backgoundColor": "#0066FF",
+            "backgroundColor": "#0066FF",
+            "templateId": "654369c94c2fd7e46ecdf27a",
+            "status": 1
+        },
+        {
+            "id": "655c2fd50329d60fa92857bb",
+            "typeContact": "bank",
+            "keyContact": "970422",
+            "nameContact": "MB",
+            "infoDetail": "LE VAN HOANG|0070133336666",
+            "linkIcon": "/images/contact/ic-mb-S.svg",
+            "backgoundColor": "#004A99",
+            "backgroundColor": "#004A99",
+            "templateId": "65489cde8953ee0aeddc5bbf",
+            "status": 1
+        },
+        {
+            "id": "657b1deb0805e0217bbbfb35",
+            "typeContact": "social",
+            "keyContact": "facebook",
+            "nameContact": "Facebook",
+            "infoDetail": null,
+            "linkIcon": "/images/contact/ic-facebook.svg",
+            "backgoundColor": "#1877F2",
+            "backgroundColor": "#1877F2",
+            "templateId": "654369c94c2fd7e46ecdf279",
+            "status": 1
+        },
+        {
+            "id": "65644d21842daa69dd4532e5",
+            "typeContact": "social",
+            "keyContact": "tiktok",
+            "nameContact": "TikTok",
+            "infoDetail": null,
+            "linkIcon": "/images/contact/ic-tiktok.svg",
+            "backgoundColor": "#4F4F4F",
+            "backgroundColor": "#4F4F4F",
+            "templateId": "65489cde8953ee0aeddc5bb2",
+            "status": 1
+        },
+        {
+            "id": "656aa2646539ff4f1d034b72",
+            "typeContact": "bank",
+            "keyContact": "970407",
+            "nameContact": "Techcombank",
+            "infoDetail": "NGUYEN BA THAI|19030367982222",
+            "linkIcon": "/images/contact/ic-techcombank-S.svg",
+            "backgoundColor": "#ED1C24",
+            "backgroundColor": "#ED1C24",
+            "templateId": "65489cde8953ee0aeddc5bb9",
+            "status": 1
+        },
+        {
+            "id": "659394f42b38fdecd5b1edaa",
+            "typeContact": "bank",
+            "keyContact": "970418",
+            "nameContact": "BIDV",
+            "infoDetail": "Cao Thanh Lich|2207041438",
+            "linkIcon": "/images/contact/ic-bidv-S.svg",
+            "backgoundColor": null,
+            "backgroundColor": "#016B69",
+            "templateId": "654369c94c2fd7e46ecdf281",
+            "status": 1
+        }
+    ],
+    "backgrounds": [
+        "https://cdn.onthedesk.vn/@admin/Cover/4a44a44b7cec4e0e9fa06a2394d4ed34.png"
+    ],
+    "package": {
+        "id": "65180f7a0b57caa904ff50f4",
+        "packageName": "PRO"
+    },
+    "packageId": {
+        "timestamp": 0,
+        "machine": 0,
+        "pid": 0,
+        "increment": 0,
+        "creationTime": "1970-01-01T00:00:00Z"
+    },
+    "username": "admin",
+    "isOwner": true,
+    "createdAt": null,
+    "updatedAt": null,
+    "version": null
+}
+
+const package = [
+    {
+      "id": "65180e830b57caa904ff50f2",
+      "name": "STANDARD",
+      "objectTarget": "Personal",
+      "features": [
+        "- Cá nhân hoá thẻ",
+        "- Cá nhân hoá hồ sơ",
+        "- Liên kết tuỳ chọn",
+        "- Liên hệ và kết nối"
+      ],
+      "featuresId": [
+        {
+          "timestamp": 1705914440,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676811,
+          "creationTime": "2024-01-22T09:07:20Z"
+        },
+        {
+          "timestamp": 1705914523,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676813,
+          "creationTime": "2024-01-22T09:08:43Z"
+        },
+        {
+          "timestamp": 1705914600,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676814,
+          "creationTime": "2024-01-22T09:10:00Z"
+        }
+      ],
+      "index": 0,
+      "showContactPrice": false,
+      "originalPrice": 179000,
+      "promotionPrice": 139000,
+      "showCallMe": false
+    },
+    {
+      "id": "65180f7a0b57caa904ff50f4",
+      "name": "PRO",
+      "objectTarget": "Freelancer",
+      "features": [
+        "- Cá nhân hoá thẻ",
+        "- Cá nhân hoá hồ sơ",
+        "- Liên kết tuỳ chọn",
+        "- Liên hệ và kết nối",
+        "- Portfolio chuyên nghiệp"
+      ],
+      "featuresId": [
+        {
+          "timestamp": 1705914440,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676811,
+          "creationTime": "2024-01-22T09:07:20Z"
+        },
+        {
+          "timestamp": 1705914523,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676813,
+          "creationTime": "2024-01-22T09:08:43Z"
+        },
+        {
+          "timestamp": 1705914600,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676814,
+          "creationTime": "2024-01-22T09:10:00Z"
+        },
+        {
+          "timestamp": 1705914623,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676815,
+          "creationTime": "2024-01-22T09:10:23Z"
+        }
+      ],
+      "index": 0,
+      "showContactPrice": false,
+      "originalPrice": 249000,
+      "promotionPrice": 179000,
+      "showCallMe": false
+    },
+    {
+      "id": "65180f600b57caa904ff50f3",
+      "name": "PRO +",
+      "objectTarget": "E-commerce",
+      "features": [
+        "- Cá nhân hoá thẻ",
+        "- Cá nhân hoá hồ sơ",
+        "- Liên kết tuỳ chọn",
+        "- Liên hệ và kết nối",
+        "- Portfolio chuyên nghiệp",
+        "- Chức năng theo yêu cầu"
+      ],
+      "featuresId": [
+        {
+          "timestamp": 1705914440,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676811,
+          "creationTime": "2024-01-22T09:07:20Z"
+        },
+        {
+          "timestamp": 1705914523,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676813,
+          "creationTime": "2024-01-22T09:08:43Z"
+        },
+        {
+          "timestamp": 1705914600,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676814,
+          "creationTime": "2024-01-22T09:10:00Z"
+        },
+        {
+          "timestamp": 1705914623,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676815,
+          "creationTime": "2024-01-22T09:10:23Z"
+        },
+        {
+          "timestamp": 1705914658,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676817,
+          "creationTime": "2024-01-22T09:10:58Z"
+        }
+      ],
+      "index": 2,
+      "showContactPrice": true,
+      "originalPrice": 0,
+      "promotionPrice": 0,
+      "showCallMe": true
+    },
+    {
+      "id": "65180fa10b57caa904ff50f5",
+      "name": "BUSINESS",
+      "objectTarget": "Corporation",
+      "features": [
+        "- Tư vấn giải pháp",
+        "- Thiết kế thẻ đồng bộ",
+        "- Hồ sơ online theo yêu cầu",
+        "- Chức năng theo yêu cầu",
+        "- Đồng hành cùng doanh nghiệp"
+      ],
+      "featuresId": [
+        {
+          "timestamp": 1705915189,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676826,
+          "creationTime": "2024-01-22T09:19:49Z"
+        },
+        {
+          "timestamp": 1705915224,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676827,
+          "creationTime": "2024-01-22T09:20:24Z"
+        },
+        {
+          "timestamp": 1705915259,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676829,
+          "creationTime": "2024-01-22T09:20:59Z"
+        },
+        {
+          "timestamp": 1705915280,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676830,
+          "creationTime": "2024-01-22T09:21:20Z"
+        },
+        {
+          "timestamp": 1705915306,
+          "machine": 6078673,
+          "pid": 31847,
+          "increment": 676831,
+          "creationTime": "2024-01-22T09:21:46Z"
+        }
+      ],
+      "index": 3,
+      "showContactPrice": true,
+      "originalPrice": 0,
+      "promotionPrice": 0,
+      "showCallMe": true
+    }
+  ]
+
+    
